bd899f194c8bc8997fdef2966956c0c50a27bf16
rakut <rakutaro56@gmail.com>
2026-03-20 01:13:29 +0900

    fix typescript error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
 
-async function callAPI(messages, system) {
+async function callAPI(messages: any[], system: string) {
   const res = await fetch("/api/chat", {
     method: "POST",
     headers: { "content-type": "application/json" },
